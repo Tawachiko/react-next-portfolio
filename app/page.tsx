3d531eff5e9e665f45c1a3d7acc2df3c4b92057f
Tawachiko <167883689+Tawachiko@users.noreply.github.com>
2024-11-09 12:37:00 +0900

    p65 coutine

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 
+import ButtonLink from "@/app/_components/ButtonLink";
+
 type News = {
   id: string;
   title: string;
@@ -13,7 +15,7 @@ type News = {
 
 const data: {
   contents: News[] } ={
-    contents:: [
+    contents: [
       {
         id: "1",
         title:
@@ -27,21 +29,63 @@ const data: {
       
     ]
   }
-}
-export default function home() {
-  const name = "世界";
+
+export default function Home () {
+  const sliceData = data.contents.slice(0,2);
+
   return(
-  <section className={styles.top}>
-  <div>
-    <h1 className="title"> テクノロジーの力で世界を変える</h1>
-    <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです</p>
-  </div>
-    <Image className={styles.bgimg}
-    src="img-mv.jpg" 
-    alt=""
-    width={4000}
-    height={1200} 
-    />
-  </section>
+    <>
+      <section className={styles.top}>
+      <div>
+        <h1 className="title"> テクノロジーの力で世界を変える</h1>
+        <p className={styles.description}>私たちは市場をリードしているグローバルテックカンパニーです</p>
+      </div>
+        <Image className={styles.bgimg}
+        src="img-mv.jpg" 
+        alt=""
+        width={4000}
+        height={1200} 
+        />
+      </section>
+      <section className={styles.news}>
+        <h2 className={styles.newstitle}>News</h2>
+        <ul>
+          {sliceData.map((article)  => (
+            <li key={article.id} className={styles.list}>
+              <div className={styles.link}>
+                <Image
+                 className={styles.image}
+                 src="/no-image.png"
+                 alt="No Image"
+                 width={1200}
+                 height={630}
+                 />
+                 <dl className={styles.content}>
+                  <dt className={styles.newsItemTitle}>
+{article.title}</dt>
+                  <dd className={styles.meta}>
+                    <span className={styles.tag}>
+{article.category.name}</span>
+                    <span className={styles.data}>
+                      <Image
+                         src="/clock.svg"
+                         alt=""
+                         width={16}
+                         height={16}
+                         priority
+                      />
+                      {article.publishedAt}
+                      </span>
+                  </dd>
+                 </dl>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっと見る</ButtonLink>
+        </div>
+      </section>
+  </>
   );
 }
