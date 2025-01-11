e810d59cc6b4055cae068f668be24c0d1471b7cc
Tawachiko <167883689+Tawachiko@users.noreply.github.com>
2025-01-11 12:21:53 +0900

    Remove unused vars

--- app/_components/ButtonLink/index.tsx
+++ app/_components/ButtonLink/index.tsx
@@ -1,16 +1,4 @@
-import Image from "next/image";
-
 import styles from "./index.module.css";
-
-type News = {
-    id: string;
-    title: string;
-    category: {
-        name: string;
-    };
-    publishedAt: string;
-    createAt: string;
-};
 
 type Props = {
     href: string;
